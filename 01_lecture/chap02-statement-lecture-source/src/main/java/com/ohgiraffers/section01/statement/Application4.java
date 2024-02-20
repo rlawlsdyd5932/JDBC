1b7808cc6db690fc1fa11981fe050926a9a357fc
DESKTOP-9BHT1C7\803-00 <koala.ohgiraffers@gmail.com>
2024-02-20 12:46:29 +0900

    statement

--- 01_lecture/chap02-statement-lecture-source/src/main/java/com/ohgiraffers/section01/statement/Application4.java
+++ 01_lecture/chap02-statement-lecture-source/src/main/java/com/ohgiraffers/section01/statement/Application4.java
@@ -33,6 +33,25 @@ public class Application4 {
             stmt = con.createStatement();
             rset = stmt.executeQuery(query);
 
+            if(rset.next()) {
+                selectedEmp = new EmployeeDTO();
+
+                selectedEmp.setEmpId(rset.getString("EMP_ID"));
+                selectedEmp.setEmpName(rset.getString("EMP_NAME"));
+                selectedEmp.setEmpNo(rset.getString("EMP_NO"));
+                selectedEmp.setEmail(rset.getString("EMAIL"));
+                selectedEmp.setPhone(rset.getString("PHONE"));
+                selectedEmp.setDeptCode(rset.getString("DEPT_CODE"));
+                selectedEmp.setJobCode(rset.getString("JOB_CODE"));
+                selectedEmp.setSalLevel(rset.getString("SAL_LEVEL"));
+                selectedEmp.setSalary(rset.getInt("SALARY"));
+                selectedEmp.setBonus(rset.getDouble("BONUS"));
+                selectedEmp.setManagerId(rset.getString("MANAGER_ID"));
+                selectedEmp.setHireDate(rset.getDate("HIRE_DATE"));
+                selectedEmp.setEntDate(rset.getDate("ENT_DATE"));
+                selectedEmp.setEntYn(rset.getString("ENT_YN"));
+            }
+
         } catch (SQLException e) {
             throw new RuntimeException(e);
         } finally {
@@ -40,5 +59,7 @@ public class Application4 {
             close(stmt);
             close(con);
         }
+
+        System.out.println("selectedEmp = " + selectedEmp);
     }
 }
