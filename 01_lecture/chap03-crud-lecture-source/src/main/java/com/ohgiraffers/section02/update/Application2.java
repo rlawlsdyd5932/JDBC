c96359be3540f42c1b33f1093f8ceea60ac44ef1
DESKTOP-9BHT1C7\803-00 <koala.ohgiraffers@gmail.com>
2024-02-21 12:52:41 +0900

    chap03-crud-update

---
+++ 01_lecture/chap03-crud-lecture-source/src/main/java/com/ohgiraffers/section02/update/Application2.java
@@ -0,0 +1,67 @@
+package com.ohgiraffers.section02.update;
+
+import com.ohgiraffers.model.dto.MenuDTO;
+
+import java.io.FileInputStream;
+import java.io.IOException;
+import java.sql.Connection;
+import java.sql.PreparedStatement;
+import java.sql.SQLException;
+import java.util.Properties;
+import java.util.Scanner;
+
+import static com.ohgiraffers.common.JDBCTemplate.close;
+import static com.ohgiraffers.common.JDBCTemplate.getConnection;
+
+public class Application2 {
+
+    public static void main(String[] args) {
+
+        Scanner sc = new Scanner(System.in);
+        System.out.print("변경할 메뉴 번호를 입력하세요 : ");
+        int menuCode = sc.nextInt();
+        System.out.print("변경할 메뉴의 이름을 입력하세요 : ");
+        sc.nextLine();
+        String menuName = sc.nextLine();
+        System.out.print("변경할 메뉴의 가격을 입력하세요 : ");
+        int menuPrice = sc.nextInt();
+
+        MenuDTO changedMenu = new MenuDTO();
+        changedMenu.setCode(menuCode);
+        changedMenu.setName(menuName);
+        changedMenu.setPrice(menuPrice);
+
+        Connection con = getConnection();
+
+        PreparedStatement pstmt = null;
+        int result = 0;
+
+        Properties prop = new Properties();
+
+        try {
+            prop.loadFromXML(new FileInputStream("src/main/java/com/ohgiraffers/mapper/menu-query.xml"));
+            String query = prop.getProperty("updateMenu");
+
+            pstmt = con.prepareStatement(query);
+            pstmt.setString(1, changedMenu.getName());
+            pstmt.setInt(2, changedMenu.getPrice());
+            pstmt.setInt(3, changedMenu.getCode());
+
+            result = pstmt.executeUpdate();
+
+        } catch (IOException e) {
+            throw new RuntimeException(e);
+        } catch (SQLException e) {
+            throw new RuntimeException(e);
+        } finally {
+            close(pstmt);
+            close(con);
+        }
+
+        if(result > 0) {
+            System.out.println("메뉴 변경 성공!");
+        } else {
+            System.out.println("메뉴 변경 실패!");
+        }
+    }
+}
